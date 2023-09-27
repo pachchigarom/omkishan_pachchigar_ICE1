fd12b0f7eaa27a445b20f3d7e2cc5843ac9a3d0b
ompac <ompac@192.168.2.105>
2023-09-27 16:58:26 -0400

    Version 1

--- src/card/Card.java
+++ src/card/Card.java
@@ -13,40 +13,45 @@ package card;
  * add your name as a modifier.
  * @author srinivsi
  */
-public class Card {
-
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
-
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
-    public String getSuit() {
-        return suit;
-    }
-
-    /**
-     * @param suit the suit to set
-     */
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
-
-    /**
-     * @return the value
-     */
+class Card {
+    
+    public static final String[] SUITS = {"Hearts", "Spades", "Clubs", "Diamonds"};
+    
+    private int value;
+    private String suit;
+    
     public int getValue() {
         return value;
     }
-
-    /**
-     * @param value the value to set
-     */
+    
     public void setValue(int value) {
         this.value = value;
     }
-   
-   
     
+    public String getSuit() {
+        return suit;
+    }
+    
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Card card = (Card) obj;
+        return value == card.value && suit.equals(card.suit);
+    }
+    
+    @Override
+    public int hashCode() {
+        int result = value;
+        result = 31 * result + suit.hashCode();
+        return result;
+    }
 }
